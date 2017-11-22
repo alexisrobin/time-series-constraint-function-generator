cc443d667640850b4f3a7daf914d71ce4dfbaf7c
alexisrobin <alexisrobin.bonjour@gmail.com>
2017-11-22 15:33:25 +0100

    :white_check_mark: Adding timing & validation to tests

--- src/test/test_func.py
+++ src/test/test_func.py
@@ -1,9 +1,24 @@
 from engine import time_series_util, features_code_generator
 from . import constraint_function
+import time
 
-#print(constraint_function.min_width_peak([4,4,2,2,3,5,5,6,3,1,1,2,2,2,2,2,2,1]))
-#print(constraint_function.min_width_peak([7,5,5,1,4,5,2,2,3,5,6,2,3,3,3,1]))
-print(constraint_function.min_surface_peak([7,5,5,1,4,5,2,2,3,5,6,2,3,3,3,1]))
-print(constraint_function.min_max_peak([7,5,5,1,4,5,2,2,3,5,6,2,3,3,3,1]))
-print(constraint_function.min_min_increasing([4,3,5,5,2,1,1,3,3,4,6,6,3,1,3,3]))
-print(constraint_function.max_range_increasing([4,3,5,5,2,1,1,3,3,4,6,6,3,1,3,3]))
+def timeAndValidateFunc(f, arg, ouput):
+    time1 = time.time()
+    ret = f(arg)
+    time2 = time.time()
+    print('TEST %s' % (f.__name__))
+    if ouput == ret:
+        print('=> PASS')
+    else:
+        print('=> FAIL')
+    print('=> function should return %d and return %d' % (ouput, ret))
+    print('=> function took %0.3f ms' % ((time2-time1)*1000.0))
+    return ret
+
+sequence1 = [7,5,5,1,4,5,2,2,3,5,6,2,3,3,3,1]
+sequence2 = [4,3,5,5,2,1,1,3,3,4,6,6,3,1,3,3]
+
+timeAndValidateFunc(constraint_function.min_surface_peak, sequence1, 9)
+timeAndValidateFunc(constraint_function.min_max_peak, sequence1, 3)
+timeAndValidateFunc(constraint_function.min_min_increasing, sequence2, 1)
+timeAndValidateFunc(constraint_function.max_range_increasing, sequence2, 2)
