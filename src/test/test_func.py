6698ef63a401a54d4d0500c7368e344ea1cbeae6
alexisrobin <alexisrobin.bonjour@gmail.com>
2017-11-27 08:56:27 +0100

    :white_check_mark: Adding some tests

--- src/test/test_func.py
+++ src/test/test_func.py
@@ -6,6 +6,7 @@ def timeAndValidateFunc(f, arg, ouput):
     time1 = time.time()
     ret = f(arg)
     time2 = time.time()
+    print('\n')
     print('TEST %s' % (f.__name__))
     if ouput == ret:
         print('=> PASS')
@@ -17,8 +18,20 @@ def timeAndValidateFunc(f, arg, ouput):
 
 sequence1 = [7,5,5,1,4,5,2,2,3,5,6,2,3,3,3,1]
 sequence2 = [4,3,5,5,2,1,1,3,3,4,6,6,3,1,3,3]
+sequence3 = [4,1,3,1,4,6,1,5,5,2,7,2,3,1,6,1]
+sequence4 = [1,7,3,4,4,5,5,4,2,2,6,5,4,6,5,7]
+sequence5 = [4,4,6,4,1,1,3,4,4,6,6,5,2,2,4,3]
+sequence6 = [1,2,3,2,5,6,7,4,1,3,4,6,1,2,4,4]
+sequence7 = [1,2,6,6,4,4,3,5,2,5,1,5,3,3,4,4]
+sequence8 = [3,4,2,2,5,6,6,4,4,3,1,1,4,6,4,4]
 
 timeAndValidateFunc(constraint_function.min_surface_peak, sequence1, 9)
 timeAndValidateFunc(constraint_function.min_max_peak, sequence1, 3)
 timeAndValidateFunc(constraint_function.min_min_increasing, sequence2, 1)
 timeAndValidateFunc(constraint_function.max_range_increasing, sequence2, 2)
+timeAndValidateFunc(constraint_function.max_range_increasing_sequence, sequence8, 5)
+timeAndValidateFunc(constraint_function.sum_surface_zigzag, sequence3, 33)
+timeAndValidateFunc(constraint_function.min_min_gorge, sequence4, 3)
+timeAndValidateFunc(constraint_function.max_surface_strictly_decreasing_sequence, sequence5, 13)
+timeAndValidateFunc(constraint_function.min_max_dip_on_increasing_sequence, sequence6, 5)
+timeAndValidateFunc(constraint_function.min_max_inflexion, sequence7, 1)
